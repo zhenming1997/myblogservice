1fd5d92988eef95324937aeb34fc5e1db0afb563
甄铭 <1976868977@qq.com>
2021-01-21 17:22:59 +0800

    项目初始化

---
+++ src/main/java/com/zhen/myblogservice/MyblogserviceApplication.java
@@ -0,0 +1,13 @@
+package com.zhen.myblogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyblogserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyblogserviceApplication.class, args);
+    }
+
+}
